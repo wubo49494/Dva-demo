d854aa4a30233d6dcde2973d38bb01e073068520
wubo49494 <wubo49494@qq.com>
2019-07-26 11:23:27 +0800

    修改注釋

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 // 1. Initialize
 // const app = dva();
 const app = dva()
+// 新建app
 
-// 2. Plugins
+// 2. Plugins  這個應該是插件
 // app.use({});
 
-// 3. Model
+// 3. Model   
 // app.model(require('./models/example').default);
+
+// 這裡很關鍵，這裡主要是存儲
 app.model(require('./models/products').default)
 
 // 4. Router
